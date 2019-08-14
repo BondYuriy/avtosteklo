67aa40ed8f76e55bedf8da525dd327bf6d9d7d4c
Bondar Yuriy <bondar.europe@gmail.com>
2019-08-14 19:36:08 +0300

    adds home page

---
+++ build/js/scripts.js
@@ -0,0 +1,12 @@
+jQuery(document).ready(function() {
+  jQuery(".vertical-center").slick({
+    dots: false,
+    arrows: false,
+    centerPadding: 0,
+    vertical: true,
+    centerMode: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true
+  });
+});
